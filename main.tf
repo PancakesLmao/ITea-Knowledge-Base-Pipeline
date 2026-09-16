adbd3945c8f9fe6b69d74ef9ed5ee6f79c21c2e3
pancakeslmao <phucthin29@gmail.com>
2026-09-17 01:03:04 +0700

    feat (infra): add buckets for app
    remove old bucket and create 2 buckets for media and search

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 # Primary resources and data sources
-module "r2_bucket" {
+module "r2_media" {
   source = "./modules/r2_bucket"
 
   account_id    = var.cloudflare_account_id
   enable_cors   = false
   location      = "apac"
-  name          = "iteakb-storage"
+  name          = "iteakb-media"
+  storage_class = "Standard"
+}
+
+module "r2_search_corpus" {
+  source = "./modules/r2_bucket"
+
+  account_id    = var.cloudflare_account_id
+  enable_cors   = false
+  location      = "apac"
+  name          = "iteakb-search-corpus"
   storage_class = "Standard"
 }
